566c87e4d26752c129ed4e1dd8a7490b0efbc650
Vitor <vitor.s.rodrigues@aluno.senai.br>
2026-09-23 15:03:18 -0300

    feat: header

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,35 +18,35 @@ export default function Header() {
 };
 
 
-const styles = StyleSheet.create({
-    header: {
-    width: '100%',
-    paddingHorizontal: 24,
-    paddingTop: 60,
-    paddingBottom: 20,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
+  const styles = StyleSheet.create({
+      header: {
+      width: '100%',
+      paddingHorizontal: 24,
+      paddingTop: 60,
+      paddingBottom: 20,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    },
 
-  headerTitle: {
-    fontSize: 24,
-    fontWeight: "800",
-    color: "#2f2d2c"
-  },
+    headerTitle: {
+      fontSize: 24,
+      fontWeight: "800",
+      color: "#2f2d2c"
+    },
 
-  headerSubtitle: {
-    fontSize: 14,
-    marginTop: 4,
-    color: "#9b9b9b"
-  },
+    headerSubtitle: {
+      fontSize: 14,
+      marginTop: 4,
+      color: "#9b9b9b"
+    },
 
-  avatarPlaceholder: {
-    width: 44,
-    height: 44,
-    borderRadius: 20,
-    backgroundColor: "#f0f0f0",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-})
+    avatarPlaceholder: {
+      width: 44,
+      height: 44,
+      borderRadius: 20,
+      backgroundColor: "#f0f0f0",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+  })
